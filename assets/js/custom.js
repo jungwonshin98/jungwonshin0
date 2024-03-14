7eaf08f0ec4761d10324f1b0d6ec7e3766372843
Jungwon Shin <Jungwon.shin98@gmail.com>
2024-03-14 15:09:51 -0500

    transferring files

---
+++ assets/js/custom.js
@@ -0,0 +1,191 @@
+
+ /* jQuery Pre loader
+  -----------------------------------------------*/
+//   $(window).load(function(){
+//     $('.preloader').fadeOut(1000); // set duration in brackets    
+// });
+
+/* Collapsible
+-----------------------------------------------*/
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    // this.text("Hide Product Description");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+ /* Google Map
+-----------------------------------------------*/
+var map = '';
+var center;
+
+function initialize() {
+    var mapOptions = {
+      zoom: 16,
+      center: new google.maps.LatLng(13.758468, 100.567481),
+      scrollwheel: false
+    };
+  
+    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+
+    google.maps.event.addDomListener(map, 'idle', function() {
+        calculateCenter();
+    });
+  
+    google.maps.event.addDomListener(window, 'resize', function() {
+        map.setCenter(center);
+    });
+}
+
+function calculateCenter() {
+  center = map.getCenter();
+}
+
+function loadGoogleMap(){
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+    document.body.appendChild(script);
+}
+
+$(function(){
+  loadGoogleMap();
+});
+
+
+/* Istope Portfolio
+-----------------------------------------------*/
+jQuery(document).ready(function($){
+
+  if ( $('.iso-box-wrapper').length > 0 ) { 
+
+      var $container  = $('.iso-box-wrapper'), 
+        $imgs     = $('.iso-box img');
+
+      $container.imagesLoaded(function () {
+
+        $container.isotope({
+        layoutMode: 'fitRows',
+        itemSelector: '.iso-box'
+        });
+
+        $imgs.load(function(){
+          $container.isotope('reLayout');
+        })
+
+      });
+
+      //filter items on button click
+
+      $('.filter-wrapper li a').click(function(){
+
+          var $this = $(this), filterValue = $this.attr('data-filter');
+
+      $container.isotope({ 
+        filter: filterValue,
+        animationOptions: { 
+            duration: 750, 
+            easing: 'linear', 
+            queue: false, 
+        }                
+      });             
+
+      // don't proceed if already selected 
+
+      if ( $this.hasClass('selected') ) { 
+        return false; 
+      }
+
+      var filter_wrapper = $this.closest('.filter-wrapper');
+      filter_wrapper.find('.selected').removeClass('selected');
+      $this.addClass('selected');
+
+        return false;
+      }); 
+
+  }
+
+});
+
+
+ /* Navigation Bar
+  -----------------------------------------------*/
+$(document).ready(function() { 
+    "use strict";
+
+    // Navbar Sticky
+
+    (function() {
+        var docElem = document.documentElement,
+            didScroll = false,
+            stickynav = 50;
+            document.querySelector( '.nav-container' );
+        function init() {
+            window.addEventListener( 'scroll', function() {
+                if( !didScroll ) {
+                    didScroll = true;
+                    setTimeout( scrollPage, 50 );
+                }
+            }, false );
+        }
+        
+        function scrollPage() {
+            var sy = scrollY();
+            if ( sy >= stickynav ) {
+                $( '.nav-container' ).addClass('sticky');
+            }
+            else {
+                $( '.nav-container' ).removeClass('sticky');
+            }
+            didScroll = false;
+        }
+        
+        function scrollY() {
+            return window.pageYOffset || docElem.scrollTop;
+        }        
+        init();        
+    })();
+
+});
+
+
+$(document).ready(function(){
+            
+    "use strict";
+//     setTimeout(function(){
+// $('.preloader').addClass('loaded');
+//     }, 5000);
+    // $('.loader').fadeOut(5000);
+    $('.body').fadeOut(2500);
+
+    $('.menu-container').each(function(index) {
+        $(this).find('.circle').attr('menu-link', index);
+        $(this).find('.list-menu').clone().appendTo('body').attr('menu-link', index);
+    });
+
+    $('.menu-container .circle').click(function() {
+        var linkedVideo = $('section').closest('body').find('.list-menu[menu-link="' + $(this).attr('menu-link') + '"]');
+        linkedVideo.toggleClass('reveal-modal');
+       
+    });
+
+    $('section').closest('body').find('.close-iframe').click(function() {
+        $(this).closest('.list-menu').toggleClass('reveal-modal');
+    });
+    
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
+
